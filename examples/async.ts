7b8be2943c290ab033644d8f44b7f80cd289d3ca
Srinivasa <m.srinivasa1234@gmail.com>
2020-07-19 18:21:32 +0530

    denofmt

--- examples/async.ts
+++ examples/async.ts
@@ -1,8 +1,13 @@
-import { loadPlugin, importAsyncFromPlugin } from 'https://denopkg.com/Srinivasa314/calcite-ts@1.0/calcite.ts';
-await loadPlugin("async", "file://target/debug/examples/")
+import {
+  loadPlugin,
+  importAsyncFromPlugin,
+} from "https://denopkg.com/Srinivasa314/calcite-ts@1.0/calcite.ts";
+await loadPlugin("async", "file://target/debug/examples/");
 
-const sleep_for = importAsyncFromPlugin("sleep_for") as (secs: number) => Promise<string>;
+const sleep_for = importAsyncFromPlugin("sleep_for") as (
+  secs: number,
+) => Promise<string>;
 
-sleep_for(5).then((response) => console.log(response))
-sleep_for(3).then((response) => console.log(response))
-sleep_for(-1).catch((err) => console.log(`Got error ${err}`))
+sleep_for(5).then((response) => console.log(response));
+sleep_for(3).then((response) => console.log(response));
+sleep_for(-1).catch((err) => console.log(`Got error ${err}`));
--- examples/sync.ts
+++ examples/sync.ts
@@ -1,18 +1,35 @@
-import { loadPlugin, importFromPlugin } from 'https://denopkg.com/Srinivasa314/calcite-ts@1.0/calcite.ts';
-await loadPlugin("sync", "file://target/debug/examples/")
+import {
+  loadPlugin,
+  importFromPlugin,
+} from "https://denopkg.com/Srinivasa314/calcite-ts@1.0/calcite.ts";
+await loadPlugin("sync", "file://target/debug/examples/");
 
-const array_buffer_example = importFromPlugin('array_buffer_example') as (arr: Int32Array, num: number) => void
-const struct_example = importFromPlugin('struct_example') as (s1: { a: number, b: string }) => void
-const return_example = importFromPlugin('return_example') as (a: [number, string]) => string
-const multiple_arguments_example = importFromPlugin('multiple_arguments_example') as (x: number, y: [string, string]) => void
-const return_buffer = importFromPlugin('return_buffer', { returnRawBuffer: true }) as () => Uint8Array
+const array_buffer_example = importFromPlugin("array_buffer_example") as (
+  arr: Int32Array,
+  num: number,
+) => void;
+const struct_example = importFromPlugin("struct_example") as (
+  s1: { a: number; b: string },
+) => void;
+const return_example = importFromPlugin("return_example") as (
+  a: [number, string],
+) => string;
+const multiple_arguments_example = importFromPlugin(
+  "multiple_arguments_example",
+) as (x: number, y: [string, string]) => void;
+const return_buffer = importFromPlugin(
+  "return_buffer",
+  { returnRawBuffer: true },
+) as () => Uint8Array;
 
 let a = new Int32Array(5);
 array_buffer_example(a, 8);
-console.log(`The value of a is ${a}`)
+console.log(`The value of a is ${a}`);
 
-struct_example({ a: 75, b: "Hey!" })
-console.log(`The result of return_example is "${return_example([98, "Bye!"])}"`)
+struct_example({ a: 75, b: "Hey!" });
+console.log(
+  `The result of return_example is "${return_example([98, "Bye!"])}"`,
+);
 
-multiple_arguments_example(8.9, ["abc", "xyz"])
-console.log(return_buffer())
+multiple_arguments_example(8.9, ["abc", "xyz"]);
+console.log(return_buffer());
